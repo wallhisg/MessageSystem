b25f580ae0e7f4532f897e12687f0711f03a8e8d
wallhisg <wallhisg@mail.com>
2018-07-13 15:18:21 +0700

    redesign message system

--- include/message/channel/gpio_channel.h
+++
@@ -1,8 +0,0 @@
-#ifndef GPIO_CHANNEL_H
-#define GPIO_CHANNEL_H
-
-#include <system/system.h>
-
-void gpio_channel_init();
-
-#endif  //  GPIO_CHANNEL_H
--- src/message/channel/gpio_channel.c
+++
@@ -1,8 +0,0 @@
-#include <message/channel/gpio_channel.h>
-#include <message/channel/channel.h>
-#include <message/message.h>
-
-void gpio_channel_init()
-{
-
-}
